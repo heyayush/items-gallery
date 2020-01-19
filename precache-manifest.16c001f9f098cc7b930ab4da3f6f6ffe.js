38ba5f1643040146b63181849e48e85bee68c37f
Ayush <ayush15may@gmail.com>
2020-01-20 00:01:13 +0530

    Updates

---
+++ precache-manifest.16c001f9f098cc7b930ab4da3f6f6ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8841c06d6535d929a87b1d34fd860e1",
+    "url": "/items-gallery/index.html"
+  },
+  {
+    "revision": "dc666924db445a9a99be",
+    "url": "/items-gallery/static/css/main.9410e2a0.chunk.css"
+  },
+  {
+    "revision": "e1d928ddc5b272e242ce",
+    "url": "/items-gallery/static/js/2.a67a7e1a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/items-gallery/static/js/2.a67a7e1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc666924db445a9a99be",
+    "url": "/items-gallery/static/js/main.71738796.chunk.js"
+  },
+  {
+    "revision": "f025e65751eec25c1baf",
+    "url": "/items-gallery/static/js/runtime-main.6bcf6f74.js"
+  }
+]);
